4d8f6649a7aa9e9fa45798911e1b4956fd895c1b
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-12 17:15:32 +0100

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const getItemTemplate = ({ preview, description, original}) =>
+    `<a class="gallery__item"  href="${original}" >
+        <img class="gallery__image" src ="${preview}" alt ="${description}">
+    </a>`;
+
+const galleryRef = document.querySelector('.gallery');
+
+const renderGallery = () => {
+  const lis = galleryItems.map((item) => getItemTemplate(item));
+  galleryRef.insertAdjacentHTML('beforeend', lis.join(''));
+};
+
+renderGallery();
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250ms', });
